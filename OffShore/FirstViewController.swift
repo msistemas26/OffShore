9bdee91ef62bcb1cf15fab58800fa0850ccd3d01
Raul Mantilla <raulmantilla@macbook-air-de-raul.local>
2019-02-13 12:51:42 -0500

    Initial Commit

---
+++ OffShore/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  OffShore
+//
+//  Created by Raul Mantilla on 13/02/19.
+//  Copyright © 2019 Raul Mantilla Assia. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ OffShore/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  OffShore
+//
+//  Created by Raul Mantilla on 13/02/19.
+//  Copyright © 2019 Raul Mantilla Assia. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
